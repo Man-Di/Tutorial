c213b4cb6598de5d3cd9b2651725fc494b5e3955
Man-Di <perfect8778@gmail.com>
2022-03-22 21:27:29 +0300

    скрипт 4.1.

--- script-4.js
+++ script-4.js
@@ -4,5 +4,29 @@ console.log(44 - 4);
 console.log("Dima" + "sik");// конкатенация - сшивание строк
 
 const putin = "zombie peoples";
-let fuckPutin = fuck + putin;
+const fuckPutin = fuck + putin;
 console.log(fuckPutin);
+
+const firstName = "Dmytro";
+const lastName = "Boichuk";
+const type = "VIP"; 
+const room = 719;
+
+
+const welcomeMsg = "Гость " + firstName + " " + lastName + " поселяется в " + type + " номер " + room;
+
+console.log(welcomeMsg);
+
+console.log(`dima nina warsawa ${180 + 180}`);
+const orderMsg = ` privet rebyata ${room}`;
+console.log(orderMsg);
+
+const welcomeMsg1 = `гость ${firstName} ${lastName} поселяется в ${type} номер ${room}`;
+console.log(welcomeMsg1);
+
+let brand = prompt("Давай бренд");
+brand = brand.toLowerCase();
+
+console.log(brand);
+
+время 1:40 в видео
